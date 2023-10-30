974b51aa0a635d35138d7447e38e69645b42aaa9
Stuart Tevendale <stuart@otag.co.uk>
2023-10-30 19:27:04 +0000

    Create AppDelegate.h

---
+++ Example-Mac/Example-Mac/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Example-Mac
+//
+//  Created by Stuart Tevendale on 30/10/2023.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
