34e0b28f759e0ad797d91aff58f4e95518b00e56
Stuart Tevendale <stuart@otag.co.uk>
2023-10-30 19:26:30 +0000

    Added Mac example project

---
+++ Example-Mac/Example-Mac/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Example-Mac
+//
+//  Created by Stuart Tevendale on 30/10/2023.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
---
+++ Example-Mac/Example-Mac/MainViewController.m
@@ -0,0 +1,23 @@
+//
+//  MainViewController.m
+//  Example-Mac
+//
+//  Created by Stuart Tevendale on 30/10/2023.
+//
+
+#import "MainViewController.h"
+
+#import <CocoaUPnP/CocoaUPnP.h>
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
